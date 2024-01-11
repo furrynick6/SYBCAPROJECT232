ba04eeab6c720e18b6cbe32b8309c2e979a28a1c
furrynick6 <rbca22232.rcca@gmail.com>
2024-01-11 11:20:53 +0530

    add commit

--- project1/src/project1/addp.java
+++ project1/src/project1/addp.java
@@ -1,9 +1,19 @@
 package project1;
 
+import java.util.Scanner;
+
 public class addp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x,y,sum;
+		
+		System.out.println("enter two numbers");
+		Scanner sc = new Scanner(System.in);
+		x=sc.nextInt();
+		y=sc.nextInt();
+		sum=x+y;
+		System.out.println("sum is : "+sum);
 
 	}
 
